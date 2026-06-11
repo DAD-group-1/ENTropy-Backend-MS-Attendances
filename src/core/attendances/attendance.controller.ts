fa589b1e95fb9d117d779726756e256c71cbae36
En0ri4n <50873744+En0ri4n@users.noreply.github.com>
2026-06-11 16:58:08 +0200

    Add logging

--- src/core/attendances/attendance.controller.ts
+++ src/core/attendances/attendance.controller.ts
@@ -15,16 +15,19 @@ export class AttendanceController {
 
   @MessagePattern({ cmd: 'create_attendance' })
   async create(@Payload() data: CreateAttendanceRequestDto) {
+    this.logger.log('Received create attendance request');
     return this.attendanceService.create(data);
   }
 
   @MessagePattern({ cmd: 'find_all_attendances' })
   findAll(query: PaginationQueryDto) {
+    this.logger.log('Received find all attendances request');
     return this.attendanceService.findAll(query);
   }
 
   @MessagePattern({ cmd: 'find_one_attendance' })
   findOne(@Payload() id: number) {
+    this.logger.log('Received find one attendance request for ID: ' + id);
     return this.attendanceService.findOne(id);
   }
 
@@ -44,11 +47,13 @@ export class AttendanceController {
       updateData: UpdateAttendanceRequestDto;
     },
   ) {
+    this.logger.log('Received update attendance request for ID: ' + payload.id);
     return this.attendanceService.update(payload.id, payload.updateData);
   }
 
   @MessagePattern({ cmd: 'remove_attendance' })
   remove(@Payload() id: number) {
+    this.logger.log('Received remove attendance request for ID: ' + id);
     return this.attendanceService.remove(id);
   }
 }
